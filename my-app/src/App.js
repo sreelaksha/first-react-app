23b06ff44cba7b5073a2615a89ddc0aa444fd9af
Sreelaksha <sreelaksha.s@gmail.com>
2020-06-15 16:59:26 +0200

    Using radium property in inline styles

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import './App.css';
+import Radium from 'radium';
 import Person from './Person/Person.js'
+
 
 class App extends Component {
     state = {
@@ -65,7 +67,11 @@ class App extends Component {
             font: 'inherit',
             border: '1px solid blue',
             padding: '8px',
-            cursor: 'pointer'
+            cursor: 'pointer',
+            ':hover' : {
+                backgroundColor : 'lightgreen',
+                color : 'black'
+            }
         };
 
     let jsPersons = null;
@@ -86,6 +92,10 @@ class App extends Component {
 
                 );
         style.backgroundColor = 'Red';
+        style[':hover'] = {
+                backgroundColor : 'salmon',
+                color: 'black'
+        }
     }
 
 
@@ -109,7 +119,7 @@ class App extends Component {
     }
 }
 
-export default App;
+export default Radium(App);
 
 
 
