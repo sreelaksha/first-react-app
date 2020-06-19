59d2c549be34ca3482afb636232e2459da708bf4
Sreelaksha <sreelaksha.s@gmail.com>
2020-06-19 22:18:11 +0200

    rendering Adjacent JSX Element - using Hoc -Higher order components- Aux

--- my-app/src/components/Persons/Person/Person.js
+++ my-app/src/components/Persons/Person/Person.js
@@ -2,20 +2,20 @@ import React, {Component} from 'react';
 //import Radium from 'radium';
 import styles from './personStyles.module.css';
 //import styled from 'styled-components';
+import Aux from '../../../hoc/Aux.js' ;
 
     class Person extends Component {
             render(){
                  console.log('Person.js Rendering...');
 
-                return [
-
-                        <p key = "i1" onClick = {this.props.clickMe}> This is {this.props.name} and i am {this.props.age} years old </p>,
-                        <p key = "i2" >{this.props.children} </p>,
-                        <input type = "text" key = "i3" onChange = { this.props.changed } value = {this.props.name}/>
-
-                     ];
-
-        }
+                return (
+                    <Aux>
+                        <p  onClick = {this.props.clickMe}> This is {this.props.name} and i am {this.props.age} years old </p>
+                        <p >{this.props.children} </p>
+                        <input type = "text"  onChange = { this.props.changed } value = {this.props.name}/>
+                    </Aux>
+                );
+    }
 
     };
 export default Person;
