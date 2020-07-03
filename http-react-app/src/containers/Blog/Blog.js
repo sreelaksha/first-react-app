a6f0fd85a959be3a8315f67f964ea14930cf3282
Sreelaksha <sreelaksha.s@gmail.com>
2020-07-03 11:38:52 +0200

    transforming data before changing the setState

--- http-react-app/src/containers/Blog/Blog.js
+++ http-react-app/src/containers/Blog/Blog.js
@@ -13,14 +13,21 @@ class Blog extends Component {
     componentDidMount(){
             axios.get('http://jsonplaceholder.typicode.com/posts')
             .then(response =>{
-                this.setState({posts: response.data });
+                const posts = response.data.slice(0,4);
+                    const updatedPost =  posts.map(post => {
+                       return{
+                        ...post,
+                        author: 'Laksha',
+                    }
+                });
+                this.setState({posts: updatedPost });
                 //console.log(response);
             });
     }
 
     render () {
         const posts = this.state.posts.map(post => {
-            return <Post key = {post.id} title = {post.title}/>;
+            return <Post key = {post.id} title = {post.title} author= {post.author}/>;
          } )
 
         return (
