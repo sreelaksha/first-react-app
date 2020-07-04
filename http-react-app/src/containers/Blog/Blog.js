38499c51eb2e66e6b4131561ab7d6758bdcde531
Sreelaksha <sreelaksha.s@gmail.com>
2020-07-04 10:03:15 +0200

    making a post selectable

--- http-react-app/src/containers/Blog/Blog.js
+++ http-react-app/src/containers/Blog/Blog.js
@@ -8,7 +8,8 @@ import './Blog.css';
 class Blog extends Component {
 
     state = {
-        posts: []
+        posts: [],
+        selectedPostId : null,
     }
     componentDidMount(){
             axios.get('http://jsonplaceholder.typicode.com/posts')
@@ -25,9 +26,18 @@ class Blog extends Component {
             });
     }
 
+    postSelectedHandler =(id) =>{
+            this.setState({ selectedPostId : id})
+    }
+
     render () {
         const posts = this.state.posts.map(post => {
-            return <Post key = {post.id} title = {post.title} author= {post.author}/>;
+            return <Post
+            key = {post.id}
+            title = {post.title}
+            author= {post.author}
+            clicked = {() => this.postSelectedHandler(post.id)}
+            />;
          } )
 
         return (
@@ -36,7 +46,7 @@ class Blog extends Component {
                     {posts}
                 </section>
                 <section>
-                    <FullPost />
+                    <FullPost id = {this.state.selectedPostId} />
                 </section>
                 <section>
                     <NewPost />
