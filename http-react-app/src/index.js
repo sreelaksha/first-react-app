3b8b2e2e17cd53bafaad978d93fe2bbdaadd8dee
Sreelaksha <sreelaksha.s@gmail.com>
2020-07-06 00:04:58 +0200

    setting a default global configuration for axios - access default header/header request types

--- http-react-app/src/index.js
+++ http-react-app/src/index.js
@@ -6,6 +6,8 @@ import * as serviceWorker from './serviceWorker';
 import axios from 'axios';
 
     axios.defaults.baseURL = 'http://jsonplaceholder.typicode.com';
+    axios.defaults.headers.common['Authorization'] = 'AUTH TOKEN';
+    axios.defaults.headers.post['Content-Type'] = 'application/json';
 
     axios.interceptors.request.use(request => {
            console.log(request);
