acb1eb484525e5fe39167f3c285909099de8f2f3
Sreelaksha <sreelaksha.s@gmail.com>
2020-07-07 23:16:36 +0200

    moved folders FullPost and New Post folders to containers

--- http-react-app/src/App.js
+++ http-react-app/src/App.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react';
+import { Browser Router } from 'react-router-dom';
 import Blog from './containers/Blog/Blog';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <Blog />
-      </div>
+    <Browser Router>
+          <div className="App">
+            <Blog />
+          </div>
+     </Browser Router>
     );
   }
 }
