b48aef2c5b29b0b932fb5504a393266aa652717d
Sreelaksha <sreelaksha.s@gmail.com>
2020-07-08 22:50:45 +0200

    removed withRouter hoc

--- http-react-app/src/components/Post/Post.js
+++ http-react-app/src/components/Post/Post.js
@@ -1,11 +1,9 @@
 import React from 'react';
-import { withRouter} from 'react-router-dom'
 
 import './Post.css';
 
-const post = (props) => {
-    console.log(props);
-    return(
+const post = (props) => (
+
         <article className="Post" onClick = {props.clicked}>
             <h1> {props.title} </h1>
             <div className="Info">
@@ -13,6 +11,5 @@ const post = (props) => {
             </div>
         </article>
     );
-};
 
-export default withRouter(post);
+export default post;
