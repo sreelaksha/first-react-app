c0db0d11f8bf07b15a152e83a15cc8ff24b1fdca
Sreelaksha <sreelaksha.s@gmail.com>
2020-07-08 16:50:44 +0200

    setting up and rendering Routes

--- http-react-app/src/containers/Blog/Blog.js
+++ http-react-app/src/containers/Blog/Blog.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 //import axios from 'axios';
 import './Blog.css';
 import Posts from '../Posts/Posts';
+import { Route } from 'react-router-dom';
 
 class Blog extends Component {
     render () {
@@ -15,7 +16,8 @@ class Blog extends Component {
                             </ul>
                     </nav>
                 </header>
-                    <Posts />
+                    <Route path = "/" exact render = { () => <h1> Home </h1> }/>
+                    <Route path = "/" render = { () => <h1> Home 2 </h1> }/>
             </div>
         );
     }
