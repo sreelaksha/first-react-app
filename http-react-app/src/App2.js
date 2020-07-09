275f1bac14cf4dfe789495a5789194f7704f9a45
Sreelaksha <sreelaksha.s@gmail.com>
2020-07-09 23:01:50 +0200

    lazy loading with React Suspense(16.6) using Toggle button - async rendering

--- http-react-app/src/App2.js
+++ http-react-app/src/App2.js
@@ -9,9 +9,30 @@ const Posts2 = React.lazy(() => import('./containers2/Posts2'));
 
 
 class App2 extends Component {
+    state ={
+        showPosts : false,
+    }
+
+    modeHandler = () => {
+        this.setState(prevState => {
+            return ({showPosts: !prevState.showPosts});
+        });
+    };
   render() {
+
     return (
-        <BrowserRouter>
+            <React.Fragment>
+                    <button onClick = { this.modeHandler }> Toggle Button </button>
+                    { this.state.showPosts ? (
+                            <Suspense fallback = {<div>Loading... </div>}>
+                                 <Posts2 />
+                            </Suspense>
+                            ) : (
+                                  <User2 />
+                                 )}
+
+            </React.Fragment>
+        /*<BrowserRouter>
                 <React.Fragment>
                 <nav>
                     <NavLink to = "/user2"> User Page </NavLink> &nbsp;
@@ -19,14 +40,14 @@ class App2 extends Component {
                 </nav>
                      <Route path ='/' component = {Welcome2} exact />
                       <Route path ='/user2' component = {User2} />
-                      {/*} <Route path ='/posts2' component = {Posts2} /> */}
+                      {*//*} <Route path ='/posts2' component = {Posts2} /> *//*}
                       <Route path="/posts2" render = {() => (
                             <Suspense fallback = {<div>Loading... </div>}>
                                 <Posts2 />
                             </Suspense>
                       )}/>
                  </React.Fragment>
-         </BrowserRouter>
+         </BrowserRouter>*/
     );
   }
 }
