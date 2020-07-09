bbc11011cf8a8f53ef255d4fe75d1efa553802db
Sreelaksha <sreelaksha.s@gmail.com>
2020-07-09 08:55:36 +0200

    programatically navigating the links without using <Link>

--- http-react-app/src/containers/Posts/Posts.js
+++ http-react-app/src/containers/Posts/Posts.js
@@ -33,7 +33,8 @@ class Posts extends Component{
         }
 
     postSelectedHandler =(id) =>{
-                this.setState({ selectedPostId : id})
+           // this.props.history.push({pathname : '/' + id});
+             this.props.history.push('/' +id );
     }
 
     render(){
@@ -41,14 +42,16 @@ class Posts extends Component{
                     if(!this.state.error) {
                             posts = this.state.posts.map(post => {
                                    return (
-                                   <Link to = {'/' + post.id}
-                                         key = {post.id}>
+                                   //<Link to = {'/' + post.id}>
+
                                        <Post
+                                            key = {post.id}
                                             title = {post.title}
                                             author= {post.author}
                                             clicked = {() => this.postSelectedHandler(post.id)}
                                         />
-                                    </Link> );
+                                   // </Link>
+                                  );
                             });
             }
             return(
